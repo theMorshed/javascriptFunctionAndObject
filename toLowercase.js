32b04eab95aa367a4bb9e37b8be37bd16ec8755f
theMorshed <morshedfive@gmail.com>
2022-08-08 10:21:49 +0600

    completed tolowercase function

---
+++ toLowercase.js
@@ -0,0 +1,11 @@
+/*
+২. একটা ফাংশন লিখবা যেটা যেকোন নামকে uppercase বা রেগুলার কেইস হিসেবে নিবে আর আউটপুট হিসেবে সেই নাম lowercase করে রিটার্ন করবে।
+*/
+
+// uppercase to lowercase
+function changeLowercase(name) {
+    let lowercase = name.toLowerCase();
+    return lowercase;
+}
+
+console.log(changeLowercase('MANJUR MORSHED'));
